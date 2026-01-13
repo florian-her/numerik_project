2c2d046290a669fc582fc8c617cdbe124a1ba238
Simen Cherubin <sm.cherubin@mci4me.at>
2026-01-13 13:06:01 +0100

    Update Kommentare zu calcDailyEnergy.m und calcPanelPower.m

--- calcDailyEnergy.m
+++ calcDailyEnergy.m
@@ -1,7 +1,20 @@
 function E_day = calcDailyEnergy(doy, panel_azimuth, panel_tilt, conf)
-    % 1. Sonnenauf- und Untergang holen
+    % CALCDAILYENERGY Berechnet den Tagesertrag durch numerische Integration.
+    %
+    % INPUT:
+    %   doy: day of year
+    %   panel_azimuth: Ausrichtung Panel
+    %   panel_tilt: Neigung Panel
+    %   conf: Config-Struct
+    %
+    % OUTPUT:
+    %   E_day: Tagesenergie in kWh
+
+
+    % 1. Sonnenauf- und Untergang als Integrationsgrenzen holen
     [t_rise, t_set, ~] = calcDayLength(doy, conf);
     
+    % Polarnacht prüfen
     if t_set <= t_rise
         E_day = 0; return;
     end
@@ -9,12 +22,17 @@ function E_day = calcDailyEnergy(doy, panel_azimuth, panel_tilt, conf)
     % 2. Zeitvektor erstellen (dt = 0.1h)
     time_steps = t_rise : 0.1 : t_set;
     
-    % 3. KEINE FOR-SCHLEIFE: Wir nutzen arrayfun, um die Leistung 
-    % für alle Zeitpunkte gleichzeitig zu berechnen
+    % 3. Berechnung der Leistungskurve
+    % Arrayfun auf Vektor anwenden
+    % Vorgehen:
+    %   Sonnenposition berechnen
+    %   Vektor s an calcPanelPower
+    %   Ergebnis Leistung P zu Zeitpunkt t als Vektor
     power_values = arrayfun(@(t) ...
         calcPanelPower(calcSunPosition(doy, t, conf), panel_azimuth, panel_tilt, conf), ...
         time_steps);
     
-    % 4. Integral berechnen via Trapezregel
+    % 4. Integral berechnen mit Trapezregel
     E_day = trapz(time_steps, power_values); 
+    % Einheit kWh
 end
--- calcPanelPower.m
+++ calcPanelPower.m
@@ -1,8 +1,13 @@
 function P = calcPanelPower(s_vec, panel_azimuth_deg, panel_tilt_deg, conf)
     % CALCPANELPOWER Berechnet die Leistung in kW/m^2
-    % s_vec: Sonnenvektor (aus calcSunPosition)
+    % Diese Funktion modelliert die geometrische Ausrichtung des Panels zur Sonne.
+    % Es wird das Lambert'sche Gesetz angewendet (Leistung ~ cos(Einfallswinkel)).
+    %
+    % INPUT:
+    % s_vec: Sonnenvektor 3x1 (aus calcSunPosition)
     % panel_azimuth_deg: Ausrichtung (0=Nord, 90=Ost, 180=Süd)
     % panel_tilt_deg: Neigung (0=flach, 90=vertikal)
+    % conf: Konfiguration-Struktur
     
     % 1. Umrechnung in Radiant
     az_rad = panel_azimuth_deg * conf.deg2rad;
@@ -11,7 +16,7 @@ function P = calcPanelPower(s_vec, panel_azimuth_deg, panel_tilt_deg, conf)
     % 2. Normalenvektor des Panels n berechnen
     % Koordinatensystem: x=Nord, y=Ost, z=Oben
     
-    % Z-Komponente (zeigt nach oben, wenn flach)
+    % Z-Komponente
     nz = cos(tilt_rad);
     
     % Horizontaler Anteil
@@ -21,9 +26,10 @@ function P = calcPanelPower(s_vec, panel_azimuth_deg, panel_tilt_deg, conf)
     nx = n_horiz * cos(az_rad);
     ny = n_horiz * sin(az_rad);
     
-    n_vec = [nx; ny; nz];
+    n_vec = [nx; ny; nz]; % Zusammen zu Vektor
     
-    % 3. Skalarprodukt (Projektion)
+    % 3. Skalarprodukt (Eifallswinkel)
+    % Das Skalarprodukt zweier Einheitsvektoren entspricht dem Kosinus des eingeschlossenen Winkels.
     cos_theta = dot(n_vec, s_vec);
     
     % 4. Leistung berechnen
@@ -31,8 +37,7 @@ function P = calcPanelPower(s_vec, panel_azimuth_deg, panel_tilt_deg, conf)
     if cos_theta < 0
         P = 0;
     else
-        % Leistung = S0 * cos(theta) * Wirkungsgrad (hier 1 angenommen)
-        % Laut Aufgabe: "Leistung... sei 1 kW/m² falls n || s" -> S0 = 1
+        % Leistung = S0 * cos(theta) * Wirkungsgrad
         P = conf.S0 * cos_theta;
     end
 end
