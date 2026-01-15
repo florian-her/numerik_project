6d7c52c27cfcacc26d6a5401934de78cd3337a3a
florian-her <hf1236@mci4me.at>
2026-01-15 10:31:38 +0100

    Task Optimierung auf SolarLib geupdated

--- TaskOptimierung.m
+++ TaskOptimierung.m
@@ -2,7 +2,6 @@
 %%Standorteinstellungen werden geladen, 4 Testtage werden def. , options
 %%für optimieren -> sauberer Terminal 
 clear; clc;
-conf = getSolarConfig();
 test_days = [80, 172, 264, 355];
 test_names = {'21. Maerz', '21. Juni', '21. Sept', '21. Dez'};
 options = optimset('Display', 'off');
@@ -13,8 +12,8 @@ fprintf('--- Optimierung der Ausrichtung (Innsbruck) ---\n\n');
 %%in dem Chache, aus calc Day lenght sonneruntergang, fast_energy_calc: Berechnet Tagesenergie für einen
 %%bestimmten Winkel, aus Azimut und Neigung -> Vektor erstellen, Vergleicht
 %%dann Panel Vektor mit Sonnenvektor
-get_day_cache = @(doy) createDayCache(doy, conf);
-fast_energy_calc = @(x, cache) calculateEnergyFast(x, cache, conf);
+get_day_cache = @(doy) SolarLib.createDayCache(doy);
+fast_energy_calc = @(x, cache) SolarLib.calculateEnergyFast(x, cache);
 
 fprintf('Berechne Sonnenvektoren für Testtage... ')
 days_cache = arrayfun(get_day_cache, test_days, 'UniformOutput', false);
@@ -25,7 +24,7 @@ run_opt = @(idx) fminsearch(@(x) -fast_energy_calc(x, days_cache{idx}), [180, 30
 
 x_matrix = cell2mat(x_opts');
 energies = -cell2mat(fvals');
-E_horiz = arrayfun(@(doy) calcDailyEnergy(doy, 180, 0, conf), test_days);
+E_horiz = arrayfun(@(doy) SolarLib.calcDailyEnergy(doy, 180, 0), test_days);
 
 Tabelle_Tage = table(test_names', x_matrix(:,1), x_matrix(:,2), energies, (energies ./ E_horiz'), ...
     'VariableNames', {'Datum', 'Opt_Azimut', 'Opt_Tilt', 'Energie_kWh', 'Faktor_vs_Flach'})
@@ -58,27 +57,3 @@ fprintf('Zusätzlicher Ertrag durch Optimierung: %.2f %%\n', improvement_annual)
 save('results_opt.mat', 'x_year'); 
 fprintf('Ergebniswurde in "results_opt.mat" gespeichert.\n');
 
-function cache = createDayCache(doy, conf)
-    [t_rise, t_set, ~] = calcDayLength(doy, conf);
-    if t_set <= t_rise
-        cache.valid = false;
-        cache.time = [];
-        cache.s_matrix = [];
-        return;
-    end
-    t = t_rise : 0.1 : t_set;
-    s_cells = arrayfun(@(time) calcSunPosition(doy, time, conf), t, 'UniformOutput', false);
-    cache.valid = true;
-    cache.time = t;
-    cache.s_matrix = cell2mat(s_cells);
-end
-
-function E = calculateEnergyFast(x, cache, conf)
-    if ~cache.valid, E = 0; return; end
-    az_rad = x(1) * conf.deg2rad;
-    tilt_rad = x(2) * conf.deg2rad;
-    n_vec = [sin(tilt_rad)*cos(az_rad); sin(tilt_rad)*sin(az_rad); cos(tilt_rad)];
-    cos_theta = n_vec' * cache.s_matrix;
-    cos_theta(cos_theta < 0) = 0; 
-    E = trapz(cache.time, conf.S0 * cos_theta);
-end
