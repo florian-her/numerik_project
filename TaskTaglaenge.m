790be19b02588fdbbebc923246e568af4d87a582
Simen Cherubin <sm.cherubin@mci4me.at>
2026-01-19 21:00:44 +0100

    Kommentare vervollständigen

--- TaskTaglaenge.m
+++ TaskTaglaenge.m
@@ -1,15 +1,19 @@
 clear;
 
-set(0, 'DefaultFigureRenderer', 'painters');
+set(0, 'DefaultFigureRenderer', 'painters'); % Design mit KI verbessert
 
 days = 1:365;
+
+% Vektor erstellen
 lengths = zeros(1, 365);
 
 for doy = days
+    % Tageslänge für jeden Tag berechnen
     [~, ~, day_length] = SolarLib.calcDayLength(doy); 
     lengths(doy) = day_length;
 end
 
+% Graph für Tageslänge übers Jahr erstellen
 figure('Name', 'Diagramm Taglaengen');
 plot(days, lengths, 'b-', 'LineWidth', 2);
 grid on;
--- TaskVektorCheck.m
+++ TaskVektorCheck.m
@@ -1,10 +1,13 @@
 clear;
 
+% Test Tag und Uhrzeit
 doy = 172;
 t = 12.0;
 
+% Sonnenposition für diesen Tag
 [s_vec, alpha, alpha_Z] = SolarLib.calcSunPosition(doy, t);
 
+% Längen kontrolle
 vektor_laenge = norm(s_vec);
 
 fprintf('--- Check Sonnenvektor ---\n');
